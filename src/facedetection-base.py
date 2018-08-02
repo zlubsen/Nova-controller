42163b9095829fd1373775b13f675e2fb02d4f64
zlubsen <zeeger@lubsen.eu>
2018-08-02 13:55:45 +0200

    Version that does not really work due to (presumed) timing issues
    Adapt to work in with SYN/ACK style communication, and using defined messages as rudimentary implemented on the arduino.

--- src/facedetection-base.py
+++ src/facedetection-base.py
@@ -1,9 +1,10 @@
 import cv2
 import serial
 import sys
+import time
 
-coordinate_correction_x = 100/355
-coordinate_correction_y = 100/266
+coordinate_correction_x = 100 / 177 #100/355
+coordinate_correction_y = 100 / 133 #100/266
 
 def setupSerial():
     ser = serial.Serial()
@@ -16,7 +17,10 @@ def setupFaceRecognition():
     return cv2.CascadeClassifier("../data/haarcascades/haarcascade_frontalface_default.xml")
 
 def setupVideoOutput():
-    return cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+    return cap
 
 def captureFrame(video):
     ret, frame = video.read()
@@ -35,19 +39,23 @@ def detectFaces(frame, faceCascade):
     return faces
 
 def writeToNova(ser, center_x, center_y):
-    ser.write(center_x * coordinate_correction_x)
-    ser.write(center_y * coordinate_correction_y)
+    x = int(center_x * coordinate_correction_x)
+    y = int(center_y * coordinate_correction_y)
+    #print("send: {0}, {1}".format(x,y))
+    ser.write(x.to_bytes(1,'big'))
+    ser.write(y.to_bytes(1,'big'))
 
 def drawDetectedFaceHighlight(frame, face):
     x, y, w, h = face
     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 def loop(ser, video_capture, face_cascade):
+    lastTime = int(round(time.time() * 1000))
     while True:
         frame = captureFrame(video_capture)
         faces = detectFaces(frame, face_cascade)
 
-        for x,y,w,h in faces:
+        for (x,y,w,h) in faces:
             centerX = x + w/2;
             centerY = y + h/2;
 
@@ -62,6 +70,12 @@ def loop(ser, video_capture, face_cascade):
 
         if ser.in_waiting > 0:
             print(ser.readline().decode("utf-8"))
+
+        currentTime = int(round(time.time() * 1000))
+        duration = currentTime - lastTime
+        lastTime = currentTime
+        print("loop took: {}".format(duration))
+#        time.sleep(1)
 
 def main():
     ser = setupSerial()
