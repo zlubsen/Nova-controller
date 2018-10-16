c72b2c0130cf271ecc747134891b2730046df62c
zlubsen <zeeger@lubsen.eu>
2018-10-16 20:17:57 +0200

    Trial for telemetry

---
+++ src/communication/serial_communication_telemetry.py
@@ -0,0 +1,100 @@
+import serial
+import sys
+from collections import deque
+from config.constants import NovaConstants
+from config.config import NovaConfig
+from utils.commandtype_enum import CommandType
+from utils.frequencytimer import FrequencyTimer
+
+class SerialCommunication:
+    def __init__(self):
+        self.connection_timer = FrequencyTimer(NovaConfig.SERIAL_RECONNECT_MS)
+        self.ser = serial.Serial()
+        self.ser.port = self.__determineSerialPort() # TODO only work on windows for now
+        self.ser.baudrate = NovaConfig.SERIAL_BAUDRATE
+        self.__open()
+
+        # for receiving data
+        self.recvInProgress = False
+        self.newData = False
+        self.receivedBytes = []
+        self.receivedCommands = deque()
+
+        self.file = open("telemetry.txt", 'w', newline='\r\n') # open telemetry
+
+    def __open(self):
+        try:
+            self.ser.open()
+            self.connected = True
+        except serial.serialutil.SerialException:
+            self.connected = False
+            print("[cntr] Failed to open serial connection to Nova")
+
+    def close(self):
+        self.ser.close()
+        self.file.close() # close telemetry
+
+    def run(self):
+        if self.connected:
+            self.__recvBytesWithStartEndMarkers()
+            self.__parseInput()
+        elif self.connection_timer.frequencyElapsed():
+            self.__open()
+
+    def commandAvailable(self):
+        return len(self.receivedCommands) > 0
+
+    def readCommand(self):
+        return self.receivedCommands.popleft()
+
+    def writeCommand(self, modcode, opcode, args):
+            cmd_content = [modcode, opcode] + args
+            command = NovaConstants.CMD_TEMPLATE.format(*cmd_content)
+            if self.connected:
+                self.ser.write(command.encode())
+            self.__printOutgoingCommand(command)
+
+    def __recvBytesWithStartEndMarkers(self):
+        while(self.connected and self.ser.in_waiting > 0 and not self.newData):
+            receivedByte = self.ser.read()
+
+            if self.recvInProgress:
+                if receivedByte is not NovaConstants.CMD_END_MARKER.encode():
+                    self.receivedBytes.append(receivedByte)
+                else:
+                    self.recvInProgress = False
+                    self.newData = True
+            elif receivedByte is NovaConstants.CMD_START_MARKER.encode():
+                self.recvInProgress = True
+
+    def __parseInput(self):
+        if self.newData:
+            cmdFields = ''.join([byte.decode() for byte in self.receivedBytes]).split(NovaConstants.CMD_SEPARATOR)
+            cmd = [CommandType.NOVA] + cmdFields
+            self.newData = False
+            self.receivedBytes.clear()
+            if cmdFields[0] == "99":
+                self.__storeTelemetry(cmdFields)
+            else:
+                self.receivedCommands.append(tuple(cmd))
+                self.__printIncomingCommand(tuple(cmd))
+
+    def __printIncomingCommand(self, command):
+        parts = list(command)
+        parts[0] = str(parts[0])
+        print("[nova] " + ':'.join(parts))
+
+    def __printOutgoingCommand(self, command):
+        print("[cntr] " + command)
+
+    def __storeTelemetry(self, cmd):
+        self.file.write(",".join(list(cmd)) + "\n")
+        self.file.flush()
+
+    def __determineSerialPort(self):
+        if sys.platform.startswith('win32'):
+            return NovaConfig.SERIAL_PORT_WINDOWS
+        elif sys.platform.startswith('darwin'):
+            return NovaConfig.SERIAL_PORT_MACOS
+        elif sys.platform.startswith('linux'):
+            return NovaConfig.SERIAL_PORT_LINUX
